7af2a37ac55430425be4131dca2d04145006c38f
TornikeALT <tornikealtunashvili@yahoo.com>
2023-07-16 16:15:45 +0400

    changed all items displaying logic, added some styles to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { CartContext } from '../context/cart-context';
 import styles from '../styles/Cart.module.css';
+import { BsFillTrashFill } from 'react-icons/bs';
 
 function Cart() {
   const {
@@ -24,35 +25,64 @@ function Cart() {
     deleteFromCart(itemId);
   };
 
+  const calculateTotalItems = () => {
+    let total = 0;
+    items.forEach(item => {
+      return (total += item.quantity);
+    });
+    return total;
+  };
+
   return (
     <>
-      {items.length === 0 && <h2>Your Shopping Cart is empty</h2>}
+      {items.length === 0 && (
+        <h2 style={{ textAlign: 'center' }}>Your Shopping Cart is empty</h2>
+      )}
       {items.length !== 0 && (
-        <div>
-          <h2>Your Shopping Cart</h2>
-          <div>
+        <div className={styles.container}>
+          <h2 className={styles.cart_title}>
+            Your Cart ({calculateTotalItems()} items)
+          </h2>
+          <div className={styles.dashedBottom}>
             {items.map(item => {
               return (
                 <div key={item.id} className={styles.cart}>
-                  <h2>{item.title}</h2>
-                  <img src={item.image} alt="" />
-                  <p>${item.price}</p>
-                  <div className={styles.qty}>
-                    <h3>Quantity</h3>
-                    <button onClick={() => removeFromCart(item.id)}>-</button>
-                    <p>1</p>
-                    <button onClick={() => addToCart(item)}>+</button>
+                  <div>
+                    <img src={item.image} alt="" className={styles.cart_img} />
                   </div>
 
-                  <p>Selected Quantity: {item.quantity}</p>
-                  <button onClick={() => handleDeleteItemClick(item.id)}>
-                    X
-                  </button>
+                  <div className={styles.cart_content}>
+                    <p className={styles.item_title}>{item.title}</p>
+
+                    <div className={styles.qty}>
+                      <h3>Quantity</h3>
+                      <button onClick={() => removeFromCart(item.id)}>-</button>
+                      <p> {item.quantity}</p>
+                      <button onClick={() => addToCart(item)}>+</button>
+                      <p>Price: ${item.price}</p>
+                    </div>
+
+                    <div className={styles.trash_icon}>
+                      <BsFillTrashFill
+                        className={styles.trash}
+                        size={20}
+                        color="purple"
+                        onClick={() => handleDeleteItemClick(item.id)}
+                      >
+                        X
+                      </BsFillTrashFill>
+                    </div>
+                  </div>
                 </div>
               );
             })}
           </div>
-          <p>TOTAL PRICE: $ {calculateTotalPrice().toFixed(2)}</p>
+          <div className={styles.checkout}>
+            <h2 className={styles.total_price}>
+              TOTAL PRICE: $ {calculateTotalPrice().toFixed(2)}
+            </h2>
+            <button>Go to Checkout</button>
+          </div>
         </div>
       )}
     </>
